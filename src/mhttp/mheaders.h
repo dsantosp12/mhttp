3b0bc930d03816be1c3c9b7e81bb698f9d644659
Daniel Santos <dsantosp12@gmail.com>
2019-04-28 18:33:44 -0400

    Added iterator to headers

--- src/mhttp/mheaders.h
+++ src/mhttp/mheaders.h
@@ -10,13 +10,34 @@
 
 namespace mhttp {
   class MHeaders {
+    using HeaderMap = std::map<std::string, std::string>;
     public:
       void set(const std::string& key, const std::string& value);
 
       MOptString get(const std::string& key) const;
 
+      inline HeaderMap::iterator begin() {
+        return _headers.begin();
+      }
+
+      inline HeaderMap::iterator end() {
+        return _headers.end();
+      }
+
+      inline HeaderMap::const_iterator cbegin() const {
+        return _headers.cbegin();
+      }
+
+      inline HeaderMap::const_iterator cend() const {
+        return _headers.cend();
+      }
+
+      inline std::size_t size() const {
+        return _headers.size();
+      }
+
     private:
-      std::map<std::string, std::string> _headers;
+      HeaderMap _headers;
   };
 }
 
